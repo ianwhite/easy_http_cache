74b32a2b9c19b1c6048ed0897e01bfadcbdf4c71
Ian White <ian.w.white@gmail.com>
2010-07-23 09:43:58 +0100

    Fixes bug: HttpCacheFilter should call #flash on the controller argument

--- lib/easy_http_cache.rb
+++ lib/easy_http_cache.rb
@@ -33,7 +33,7 @@ module ActionController #:nodoc:
         end
 
         def filter(controller)
-          return unless flash.empty? && controller.request.get?
+          return unless controller.send(:flash).empty? && controller.request.get?
 
           last_modified = get_last_modified(controller)
           controller.response.last_modified = last_modified if last_modified
